01ee0e87e15b9df0d4072868930bbd367626dd6a
Abhishek <Abhishek>
2016-03-30 14:50:24 +0530

    content change and create a new blog page

--- wp-content/themes/rosa/blog-post.php
+++ wp-content/themes/rosa/blog-post.php
@@ -33,7 +33,7 @@ if ( post_password_required() && ! $wpgrade_private_post['allowed'] ) {
 
 		if ( ! empty( $post->post_content ) ) : ?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class( $classes ); ?>>
-				<section>
+				<section class="blog-flex">
 					<div class="col-8">
 						<div class="container-fulid">
 							<section class="page__content  js-post-gallery  cf">
@@ -48,78 +48,128 @@ if ( post_password_required() && ! $wpgrade_private_post['allowed'] ) {
 								<?php $count = 0; ?>
 								<?php $count_inner = 0; ?>
 								<?php if ( $the_query->have_posts() ) : ?>
-								    <div id="isotope-list">
-									    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-											<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>
-												
-												<?php if($count % 3 == 0): ?>
-													<?php if($count_inner < 1): ?>
-														<a href="<?php echo get_permalink(); ?>">
-															<div class="grid-item grid-col-6">
-																<h3><?php the_title();?></h3>
-																<small>
-																	<?php 
-																	$terms = get_the_terms( $post->ID , 'portfolio_categories');
-																	foreach( $terms as $term ) {
-																		print $term->slug ;
-																		echo ", ";
-																		unset($term);
-																	} ?>
-																</small>
-																<div class="image-container" style="background-image: url('<?php echo $url; ?>');">
-																	<div class="linear-background"></div>
-																</div>
-															</div>
-														</a>
-														<?php $count_inner++; ?>
-													<?php else: ?>
-														<a href="<?php echo get_permalink(); ?>">
-															<div class="grid-item grid-col-6 float-right">
-																<h3><?php the_title();?></h3>
-																<small>
-																	<?php 
-																	$terms = get_the_terms( $post->ID , 'portfolio_categories');
-																	foreach( $terms as $term ) {
-																		print $term->slug ;
-																		echo ", ";
-																		unset($term);
-																	} ?>
-																</small>
-																<div class="image-container" style="background-image: url('<?php echo $url; ?>');">
-																	<div class="linear-background"></div>
-																</div>
-															</div>
-														</a>
-														<?php $count_inner = 0; ?>
-													<?php endif;?>
-														<?php $count++; ?>
-												<?php else: ?>
-													<a href="<?php echo get_permalink(); ?>">
-														<div class="grid-item grid-col-3">
-															<h3><?php the_title();?></h3>
-															<small>
-																<?php 
-																$terms = get_the_terms( $post->ID , 'portfolio_categories');
-																foreach( $terms as $term ) {
-																	print $term->slug ;
-																	echo ", ";
-																	unset($term);
-																} ?>
-															</small>
-															<div class="image-container" style="background-image: url('<?php echo $url; ?>');">
-																<div class="linear-background"></div>
-															</div>
-														</div>
-													</a>
-													<?php $count++; ?>
-												<?php endif; ?>
-									    <?php endwhile;  ?>
+								    <div id="isotope-list" class="blogs">
+								    	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+								    		<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>	
+
+								    		<?php if($count % 6 == 0): ?>
+												<?php if($count_inner < 1): ?>
+													<div class="blog-align">
+													<!-- Big Blog Thumb -->
+										    		<div class="blog-wrap">
+										    			<div class="inner-blog">
+												    		<a href="<?php echo get_permalink(); ?>">
+												    			<div class="grid-item grid-col-6">
+												    				<div class="image-container" style="background-image: url('<?php echo $url; ?>');">
+												    					<div class="linear-background"></div>
+												    				</div>
+												    			</div>
+												    		</a>
+												    		<div class="clear"></div>
+										    				<h3 class="blog-heading"><?php the_title();?></h3>
+										    				<small>
+										    					<?php 
+										    					$terms = get_the_terms( $post->ID , 'portfolio_categories');
+										    					foreach( $terms as $term ) {
+										    						print $term->slug ;
+										    						echo ", ";
+										    						unset($term);
+										    					} ?>
+										    				</small>
+										    				 <p><?php the_field('blog-text'); ?></p>
+										    				 <hr>
+										    				 <div class="admin-detail">
+										    					<?php $author_id=$post->post_author; ?>
+																<?php echo get_avatar($default); ?> 
+																<span>by <?php echo the_author_meta( 'user_nicename' , $author_id ); ?></span>
+										    				</div>
+									    				</div>
+								    				</div>
+								    			<?php $count_inner++; ?>
+
+								    		<?php else: ?>
+												<!-- Medium Blog Thumb -->
+								    			<div class="blog-wrap">
+									    			<div class="inner-blog">
+											    		<a href="<?php echo get_permalink(); ?>">
+											    			<div class="grid-item grid-col-8">
+											    				<div class="image-container" style="background-image: url('<?php echo $url; ?>');">
+											    					<div class="linear-background"></div>
+											    				</div>
+											    			</div>
+											    		</a>
+											    		<div class="clear"></div>
+									    				<h3 class="blog-heading"><?php the_title();?></h3>
+									    				<small>
+									    					<?php 
+									    					$terms = get_the_terms( $post->ID , 'portfolio_categories');
+									    					foreach( $terms as $term ) {
+									    						print $term->slug ;
+									    						echo ", ";
+									    						unset($term);
+									    					} ?>
+									    				</small>
+									    				<p><?php the_field('blog-text'); ?></p>
+									    				<hr>
+									    				<div class="admin-detail">
+									    					<?php $author_id=$post->post_author; ?>
+															<?php echo get_avatar($default); ?>
+															<span>by <?php echo the_author_meta( 'user_nicename' , $author_id ); ?></span>
+									    				</div>
+								    				</div>
+							    				</div>
+							    				<?php 
+												    $count_inner++;
+												    if($count_inner == 5) {
+												    	$count_inner = 0;
+														$count++;
+												    }
+												 ?>
+							    			<?php endif;?>
+											<?php else: ?>
+												<!-- Small Blog Thumb -->
+												<div class="blog-wrap float-right">
+									    			<div class="inner-blog">
+											    		<a href="<?php echo get_permalink(); ?>">
+											    			<div class="grid-item grid-col-3">
+											    				<div class="image-container" style="background-image: url('<?php echo $url; ?>');">
+											    					<div class="linear-background"></div>
+											    				</div>
+											    			</div>
+											    		</a>
+											    		<div class="clear"></div>
+									    				<h3 class="blog-heading"><?php the_title();?></h3>
+									    				<small>
+									    					<?php 
+									    					$terms = get_the_terms( $post->ID , 'portfolio_categories');
+									    					foreach( $terms as $term ) {
+									    						print $term->slug ;
+									    						echo ", ";
+									    						unset($term);
+									    					} ?>
+									    				</small>
+									    				<p><?php the_field('blog-text'); ?></p>
+									    				<hr>
+									    				<div class="admin-detail">
+									    					<?php $author_id=$post->post_author; ?>
+															<?php echo get_avatar($default); ?>
+															<span>by <?php echo the_author_meta( 'user_nicename' , $author_id ); ?></span>
+									    				</div>
+								    				</div>
+							    				</div>
+							    				</div>
+							    				<div class="clear"></div>
+							    				<?php $count = 0; ?>
+											<?php endif; ?>
+								    	<?php endwhile;  ?>
 								    </div> 
 								<?php endif; ?>
 							</section>
 						</div>
 					</div>
-					<div class="col-4"></div>
+					<div class="col-4">
+						<?php dynamic_sidebar( 'sidebar-main' ); ?>
+					</div>
 				</section>
 			</article>
 		<?php endif;
